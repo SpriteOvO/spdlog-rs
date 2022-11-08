90cb5a5eea1d423ed422f189447c154dc5f2527c
Sprite <SpriteOvO@gmail.com>
2022-11-08 21:08:06 +0800

    Add crate-level documentation for `spdlog-macros`

--- spdlog-macros/src/lib.rs
+++ spdlog-macros/src/lib.rs
@@ -1,3 +1,10 @@
+//! Macros implementation of crate [`spdlog-rs`].
+//!
+//! `spdlog-macros` should currently not be used as a standalone dependency, use
+//! [`spdlog-rs`] instead.
+//!
+//! [`spdlog-rs`]: https://crates.io/crates/spdlog-rs
+
 mod helper;
 mod parse;
 mod synthesis;
